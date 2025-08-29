d06dd58a65610efa048ac30ea3162801f6041d85
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-29 01:44:34 -0300

    Add Header component with layout for Smart Market

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center p-4 bg-white border-b border-gray-200 shadow-sm">
+            <h1 className="text-2xl font-bold text-black container mx-auto">Smart Market</h1>
+        </header>
+    )
+}
