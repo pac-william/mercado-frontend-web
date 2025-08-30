c02466f2044f513390a882013c51cafc4c75f4f6
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-30 02:14:31 -0300

    Enhance ProductCard component: add Separator for improved layout, adjust flex properties for better responsiveness, and refine styling for market and product details.

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import InputPlusMinus from "@/components/input-plus-minus";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { Market, Product } from "@/lib/mock-data";
 import { ShoppingCart, Star } from "lucide-react";
 import Image from "next/image";
@@ -22,13 +23,15 @@ export default function ProductCard({ market, product }: ProductCardProps) {
     };
 
     return (
-        <Card className="flex flex-col">
-            <CardHeader className="flex flex-row gap-2 border-b">
+        <Card className="flex flex-col flex-1">
+            <CardHeader className="flex flex-1 flex-row gap-2">
                 <Image src={`/mercado-frontend-web${market.profilePicture}`} alt="Product" width={100} height={100} className="object-cover rounded-full w-16 h-16 shadow-md border" />
-                <div className="bg-white rounded-full flex flex-col gap-1">
-                    <span className="text-sm font-bold">{market.name}</span>
-                    <span className="text-xs line-clamp-1">{market.address}</span>
-                    <div className="flex flex-row gap-1">
+                <div className="bg-white rounded-full flex flex-1 flex-col gap-2">
+                    <div className="flex flex-col">
+                        <span className="text-sm font-bold">{market.name}</span>
+                        <span className="text-xs line-clamp-1">{market.address}</span>
+                    </div>
+                    <div className="flex flex-1 flex-row gap-1">
                         <Star size={16} className="text-yellow-500" />
                         <Star size={16} className="text-yellow-500" />
                         <Star size={16} className="text-yellow-500" />
@@ -38,11 +41,13 @@ export default function ProductCard({ market, product }: ProductCardProps) {
                 </div>
             </CardHeader>
 
+            <Separator />
+
             <CardContent className="p-0 relative w-full aspect-square">
                 <Image src={`https://picsum.photos/256/256?random=${product.id}`} alt="Product" fill className="object-cover" />
             </CardContent>
 
-            <CardFooter className="flex flex-col gap-2 border-t items-start">
+            <CardFooter className="flex flex-col gap-2 items-start">
                 <p className="text-sm">{product.description}</p>
                 <div className="grid grid-cols-[auto_1fr] items-center w-full">
                     <p>De</p>
@@ -56,7 +61,9 @@ export default function ProductCard({ market, product }: ProductCardProps) {
                 </div>
             </CardFooter>
 
-            <CardFooter className="flex flex-row justify-between gap-2 mt-auto">
+            <Separator />
+
+            <CardFooter className="flex flex-row justify-between gap-2">
                 <InputPlusMinus />
                 <div className="flex flex-row gap-2 items-center">
                     <span className="text-sm">Adicionar:</span>
