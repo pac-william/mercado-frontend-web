259bb4de57dad415ed2065a1724269a957be8f7b
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-29 21:24:45 -0300

    Enhance Header component: add navigation links for cart, history, and profile; wrap title in a Link for homepage navigation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { History, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <header className="flex justify-between items-center p-4 bg-white border-b border-gray-200 shadow-sm">
-            <h1 className="text-2xl font-bold text-black container mx-auto">Smart Market</h1>
+        <header className="flex w-full justify-between items-center p-4 bg-white border-b border-gray-200 shadow-sm">
+            <h1 className="text-2xl font-bold text-black container mx-auto">
+                <Link href="/">
+                    Smart Market
+                </Link>
+            </h1>
+            <div className="ml-auto flex flex-row gap-2">
+                <Button variant="outline" size="icon_lg" asChild>
+                    <Link href="/cart">
+                        <ShoppingCart size={24} />
+                    </Link>
+                </Button>
+                <Button variant="outline" size="icon_lg" asChild>
+                    <Link href="/history">
+                        <History size={24} />
+                    </Link>
+                </Button>
+                <Button variant="outline" size="icon_lg" asChild>
+                    <Link href="/profile">
+                        <User size={24} />
+                    </Link>
+                </Button>
+            </div>
         </header>
     )
 }
