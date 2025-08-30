1ff7fc22b33c9913390396f658b83f0cf8642d9a
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-29 21:27:05 -0300

    Add History page: implement purchase history display with date formatting and product details using CartMarketGroup component

---
+++ src/app/(app)/history/page.tsx
@@ -0,0 +1,34 @@
+import CartMarketGroup from "@/app/components/CartMarketGroup";
+import { products } from "@/lib/mock-data";
+import moment from "moment";
+import "moment/locale/pt-br";
+
+export default function History() {
+    const history = Array.from({ length: 3 }).map((_, index) => ({
+        date: new Date(2025, 7, 29),
+        products: products
+    }));
+    return (
+        <div className="flex flex-col gap-4 container mx-auto">
+            <h1 className="text-2xl font-bold">Histórico de compras</h1>
+            {
+                history.map((item, index) => (
+                    <div className="grid grid-cols-[auto_1fr] gap-4" key={index}>
+                        <div className="flex  gap-4">
+                            <div className="flex flex-col items-end">
+                                <span className="">{moment(item.date).calendar()}</span>
+                                <span className="text-sm text-muted-foreground">{moment(item.date).calendar()}</span>
+                            </div>
+                            <div className="flex flex-col flex-1 items-center relative">
+                                <span className={`h-4 w-[2px] bg-primary absolute -top-4 ${index === 0 ? "hidden" : ""}`}></span>
+                                <span className="min-h-4 min-w-4 border-2 border-primary bg-white rounded-full"></span>
+                                <span className={`h-full w-[2px] bg-primary absolute -bottom-4 ${index === history.length - 1 ? "hidden" : ""}`}></span>
+                            </div>
+                        </div>
+                        <CartMarketGroup marketName="Mercado 1" marketAddress="Endereço 1" products={products} />
+                    </div>
+                ))
+            }
+        </div>
+    )
+}   
