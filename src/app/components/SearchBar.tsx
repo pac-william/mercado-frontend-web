73a20862f253db52d872d0d38c7dd78f8a45945d
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-30 02:14:25 -0300

    Refactor SearchBar component: update button styles, add Mic icon, and integrate Link for search suggestions

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,17 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { SearchIcon, Sparkles } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { Mic, SearchIcon, Sparkles } from "lucide-react";
+import Link from "next/link";
 
 export default function SearchBar() {
     return (
         <Card className="flex flex-row items-center w-[500px] shadow-md">
-            <Button variant="ghost" size="icon_lg" className="rounded-r-none">
+            <Button variant="ghost" size="icon_lg" className="rounded-none">
                 <Sparkles size={24} />
             </Button>
             <Input type="text" placeholder="O que gostaria de fazer hoje?" className="h-12 rounded-none border border-y-0" />
-            <Button variant="ghost" size="icon_lg" className="rounded-l-none">
-                <SearchIcon size={24} />
+            <Button variant="ghost" size="icon_lg" className="rounded-none">
+                <Mic size={24} />
+            </Button>
+            <Separator orientation="vertical" />
+            <Button variant="ghost" size="icon_lg" className="rounded-none" asChild>
+                <Link href={`/suggestion/${1}`}>
+                    <SearchIcon size={24} />
+                </Link>
             </Button>
         </Card>
     )
