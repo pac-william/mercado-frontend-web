0662b6f81aa024004bb8122b4bdc7d888fa692a7
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-29 22:48:48 -0300

    Refactor Layout and Home components: integrate ScrollArea for improved layout, enhance layout styling for better responsiveness, and add metadata for SEO optimization

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,9 +2,9 @@ import Header from "../components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col h-screen">
             <Header />
-            <div className="flex flex-1 flex-col gap-4 mt-4">
+            <div className="flex flex-1 flex-col gap-4">
                 {children}
             </div>
         </div >
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,29 +1,39 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { getMarketById, products } from "@/lib/mock-data";
+import { Metadata } from "next";
 import HeroSection from "../components/HeroSection";
 import ProductCard from "../components/ProductCard";
 import SearchBar from "../components/SearchBar";
 
+
+export const metadata: Metadata = {
+    title: 'Smart Market',
+    description: 'Compre produtos de qualidade com preços acessíveis',
+}
+
 export default function Home() {
     return (
-        <div className="flex flex-col gap-4 items-center">
-            <HeroSection />
-            <div className="flex flex-col gap-4 items-center justify-center h-64">
-                <SearchBar />
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 container mx-auto">
-                {
-                    products.map((product) => {
-                        const market = getMarketById(product.marketId);
-                        return (
-                            <ProductCard
-                                key={product.id}
-                                market={market!}
-                                product={product}
-                            />
-                        );
-                    })
-                }
-            </div>
-        </div >
+        <ScrollArea className="flex flex-col flex-grow h-0">
+            <div className="flex flex-col gap-4 items-center my-4">
+                <HeroSection />
+                <div className="flex flex-col gap-4 items-center justify-center h-64">
+                    <SearchBar />
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 container mx-auto">
+                    {
+                        products.map((product) => {
+                            const market = getMarketById(product.marketId);
+                            return (
+                                <ProductCard
+                                    key={product.id}
+                                    market={market!}
+                                    product={product}
+                                />
+                            );
+                        })
+                    }
+                </div>
+            </div >
+        </ScrollArea>
     )
 }
