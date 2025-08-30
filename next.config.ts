79f1e1bb1c3872fa1734c12305527bc4e26d075b
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-30 02:13:58 -0300

    Remove basePath and assetPrefix from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: '/mercado-frontend-web',
-  assetPrefix: '/mercado-frontend-web/',
   images: {
     remotePatterns: [
       {
