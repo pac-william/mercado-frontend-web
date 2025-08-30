867527a38c06aaded959acffee5af25a09ba3f31
romulogdonadoni <romulogdonadoni@gmail.com>
2025-08-29 21:24:54 -0300

    Refactor SearchBar component: replace div with Card for improved layout; update button styles and input placeholder text

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { SearchIcon, Sparkle, Sparkles } from "lucide-react";
+import { SearchIcon, Sparkles } from "lucide-react";
 
 export default function SearchBar() {
     return (
-        <div className="flex items-center">
-            <Button variant="outline" size="icon_lg">
+        <Card className="flex flex-row items-center w-[500px] shadow-md">
+            <Button variant="ghost" size="icon_lg" className="rounded-r-none">
                 <Sparkles size={24} />
             </Button>
-            <Input type="text" placeholder="Pesquisar" className="h-12" />
-            <Button variant="outline" size="icon_lg">
+            <Input type="text" placeholder="O que gostaria de fazer hoje?" className="h-12 rounded-none border border-y-0" />
+            <Button variant="ghost" size="icon_lg" className="rounded-l-none">
                 <SearchIcon size={24} />
             </Button>
-        </div>
+        </Card>
     )
 }
