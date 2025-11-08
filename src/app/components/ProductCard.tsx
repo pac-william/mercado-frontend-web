a62963fa7f6ec36f34067b6f08c53605fc3cd059
romulo-donadoni <romulogdonadoni@outlook.com>
2025-11-08 17:47:25 -0300

    refactor(ProductCard): remove unused admin features and clean up imports for improved readability and maintainability

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -3,20 +3,17 @@
 import bh_supermercados from "@/../public/markets/bh_supermercados.png";
 import { addItemToCart } from "@/actions/cart.actions";
 import InputPlusMinus from "@/components/input-plus-minus";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { Edit2, Loader2, ShoppingCart, Star, Trash2 } from "lucide-react";
+import { Loader2, ShoppingCart, Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
-import { ProductCreateForm } from "../admin/products/create/components/ProductCreateForm";
 import { Product } from "../domain/productDomain";
 import { formatPrice } from "../utils/formatters";
 
@@ -104,11 +101,6 @@ export default function ProductCard({ product, variant = "buy-now", badgeText, b
             setAddingToCart(false);
         }
     }
-
-    const deleteProduct = () => {
-        toast.success("Produto deletado");
-    }
-
     switch (variant) {
         case "quantity-select":
             return (
@@ -158,18 +150,18 @@ export default function ProductCard({ product, variant = "buy-now", badgeText, b
                     <Separator />
 
                     <CardFooter className="flex flex-row justify-between gap-2">
-                        <InputPlusMinus 
-                            value={quantity} 
+                        <InputPlusMinus
+                            value={quantity}
                             onChange={(value) => {
                                 const newQuantity = value ?? 1;
                                 setQuantity(newQuantity);
                                 onQuantityChange?.(product.id, newQuantity);
-                            }} 
+                            }}
                         />
                         <div className="flex flex-row gap-2 items-end">
-                            <Button 
-                                variant="outline" 
-                                size="icon" 
+                            <Button
+                                variant="outline"
+                                size="icon"
                                 onClick={handleAddToCart}
                                 disabled={addingToCart}
                             >
@@ -230,9 +222,9 @@ export default function ProductCard({ product, variant = "buy-now", badgeText, b
                                 </div>
                             </div>
                             <div className="flex flex-row gap-2 items-end">
-                                <Button 
-                                    variant="outline" 
-                                    size="icon" 
+                                <Button
+                                    variant="outline"
+                                    size="icon"
                                     onClick={handleAddToCart}
                                     disabled={addingToCart}
                                 >
@@ -245,106 +237,6 @@ export default function ProductCard({ product, variant = "buy-now", badgeText, b
                             </div>
                         </div>
                     </CardFooter>
-                </Card>
-            );
-        case "admin":
-            return (
-                <Card className="flex flex-col max-w-xs w-full bg-card border-border">
-                    <CardHeader className="flex flex-row gap-2 items-center justify-between">
-                        <Badge variant={"outline"} className="text-card-foreground border-border">
-                            Categoria
-                        </Badge>
-                        <div className="flex flex-row gap-2 items-center">
-                            <Dialog>
-                                <DialogTrigger asChild>
-                                    <Button variant="outline" size="icon" >
-                                        <Edit2 size={16} />
-                                    </Button>
-                                </DialogTrigger>
-                                <DialogContent className="p-0 gap-0 max-w-2xl bg-background border-border">
-                                    <DialogHeader className="p-4">
-                                        <DialogTitle className="text-foreground">Editar produto</DialogTitle>
-                                        <DialogDescription className="text-muted-foreground">
-                                            Aqui você pode editar as informações do produto.
-                                        </DialogDescription>
-                                    </DialogHeader>
-                                    <Separator />
-                                    <div className="p-4 flex flex-col gap-2">
-                                        <ProductCreateForm markets={[]} />
-                                    </div>
-                                </DialogContent>
-                            </Dialog>
-
-                            <AlertDialog>
-                                <AlertDialogTrigger asChild>
-                                    <Button variant="destructive" size="icon" >
-                                        <Trash2 size={16} />
-                                    </Button>
-                                </AlertDialogTrigger>
-                                <AlertDialogContent className="p-0 gap-0 bg-background border-border">
-                                    <AlertDialogHeader className="p-4">
-                                        <AlertDialogTitle className="text-foreground">Deletar produto</AlertDialogTitle>
-                                        <AlertDialogDescription className="text-muted-foreground">
-                                            Aqui você pode deletar o produto.
-                                        </AlertDialogDescription>
-                                    </AlertDialogHeader>
-                                    <Separator />
-                                    <AlertDialogFooter className="p-4">
-                                        <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                                        <AlertDialogAction onClick={() => deleteProduct()}>Deletar</AlertDialogAction>
-                                    </AlertDialogFooter>
-                                </AlertDialogContent>
-                            </AlertDialog>
-
-                        </div>
-                    </CardHeader>
-
-                    <Separator />
-
-                    <CardHeader className="flex flex-row gap-2">
-                        <Image src={bh_supermercados} alt="Product" width={100} height={100} className="object-cover rounded-full aspect-square w-12 h-12 shadow-md border" />
-                        <div className="rounded-full flex flex-col gap-2">
-                            <div className="flex flex-col">
-                                <span className="text-sm font-bold text-card-foreground">Market 1</span>
-                                <div className="flex flex-row gap-1">
-                                    <Star size={16} className="text-yellow-500" />
-                                    <Star size={16} className="text-yellow-500" />
-                                    <Star size={16} className="text-yellow-500" />
-                                    <Star size={16} className="text-yellow-500" />
-                                    <Star size={16} className="text-yellow-500" />
-                                </div>
-                            </div>
-                        </div>
-                    </CardHeader>
-
-                    <Separator />
-
-                    <CardContent className="p-0 relative w-full aspect-square">
-                        <Image src={getImageSrc()} alt="Product" fill className="object-cover p-8" />
-                    </CardContent>
-
-                    <CardFooter className="flex flex-col gap-2 items-start">
-                        <p className="text-sm line-clamp-2 text-card-foreground">{product.name}</p>
-                        <div className="flex flex-row w-full justify-between mt-auto">
-                            <div className="grid grid-cols-[auto_1fr] items-center w-full">
-                                {
-                                    shouldShowDiscount(product.id) ? (
-                                        <>
-                                            <p className="text-card-foreground">De</p>
-                                            <div className="flex flex-row items-center ml-2">
-                                                {mountPriceView(getDiscountPrice(product.price, product.id), "old")}
-                                            </div>
-                                            <p className="text-card-foreground">Por</p>
-                                        </>
-                                    ) : null
-                                }
-                                <div className="flex flex-row items-center ml-2">
-                                    {mountPriceView(product.price, "new")}
-                                </div>
-                            </div>
-                        </div>
-                    </CardFooter>
-
                 </Card>
             );
         case "history":
@@ -430,9 +322,9 @@ export default function ProductCard({ product, variant = "buy-now", badgeText, b
                                 </div>
                             </div>
                             <div className="flex flex-row gap-2 items-end">
-                                <Button 
-                                    variant="outline" 
-                                    size="icon" 
+                                <Button
+                                    variant="outline"
+                                    size="icon"
                                     onClick={handleAddToCart}
                                     disabled={addingToCart}
                                 >
